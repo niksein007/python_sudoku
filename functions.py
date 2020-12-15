096c9fdb99721d537d9f265ff94774b41c14febc
niksein007 <markpearceniksein@gmail.com>
2020-12-15 18:39:31 +0000

    fixed bugs

--- functions.py
+++ functions.py
@@ -1,5 +1,67 @@
+from browser import document,timer
+def get_inner_box_indexes (id,boxes):
+    """
+    returns a list of the inner box, the indexes of its box, row and column
+    that is attached to it.
+    """
+    # slice out the number of the box from the id
+    box_num = id[0][3:4]
+    #convert to int and minus 1 to get box index
+    box_index = int(box_num) -1 
+
+    py_inner_box = boxes[box_index][id[0]]
+    # get column index
+    column_num = py_inner_box['column_attached'][-1]
+    column_index = int(column_num) -1
+    # row index
+    row_num = py_inner_box['row_attached'][-1]
+    row_index = int(row_num) -1
+
+    return [py_inner_box,box_index,column_index,row_index]
 
 
+def unchecked_err(boxes,columns,rows):
+    """
+    check if error class is still attached because the number removed 
+    may not be the exact one that caused the error
+    """
+    for box in boxes:
+        for i in range(1,10):
+            # get inner_boxes name and check the class value
+            py_id =box[f"{box['name']}_inner_box{i}"]['name']
+            # to get the html equivalent
+            html_id = document[box[f"{box['name']}_inner_box{i}"]['name']]
+            # print(py_id)
+            if html_id.class_name == 'error':
+                column_index = int(box[py_id]['column_attached'][6:7]) - 1
+                row_index = int(box[py_id]['row_attached'][3:4]) - 1
+                #chexk for distinct values in CRB
+                if (len(box['values']) == len(set(box['values']))) \
+                and (len(columns[column_index]['values']) == len(set(columns[column_index]['values']))) \
+                and (len(rows[row_index]['values']) == len(set(rows[row_index]['values']))):
+                    html_id.class_name = ''
+                    # print('testing unchecked error')
+
+def clear_all_values(functions,id,inner_box,boxes,columns,rows):
+    """
+    removes all values for html,box,column and row
+    """
+    if inner_box.text != '':
+        #get the inner_box and its attached box,column,row
+        box_list = functions.get_inner_box_indexes(id,boxes)
+        # print(box_list[0])
+        box_list[0]['value'] = ''
+        boxes[box_list[1]]['values'].remove(inner_box.text)
+        columns[box_list[2]]['values'].remove(inner_box.text)
+        rows[box_list[3]]['values'].remove(inner_box.text)
+        
+        inner_box.text = ''
+
+def time_counter(parameter_list):
+    """
+    calculates the time taken to solve the game
+    """
+    pass
 
 
 
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from browser import document, html, bind
 import functions
+
 # creating Rows, Columns and Boxes
 columns = []
 rows = []
@@ -11,23 +12,15 @@ for CRB in range(1, 10):  # CRB == column row and box
     column = {'name': f'column{CRB}',
               'values': []
               }
-    # for num in range(1,10):
-    #     column[f'column{CRB}'].append(num)
     columns.append(column)
 
     row = {'name': f'row{CRB}',
            'values': []
            }
-    # for num in range(1,10):
-    #     row[f'row{CRB}'].append(num)
     rows.append(row)
 
     box = {'name': f'box{CRB}',
-        #    'columns_attached': [],
-        #    'rows_attached': [],
            'values': [],
-        #    'tag':f"<div id='box{CRB}' class='box'></div>"
-
            }
     # attach 9 inner divs/boxes
     for num in range(1, 10):
@@ -38,13 +31,6 @@ for CRB in range(1, 10):  # CRB == column row and box
             'value':'',
         }
     boxes.append(box)
-
-    # print(column)
-    # print(row)
-    # print(box)
-
-
-
 
 # step 2 attach row and column to innerboxes
 ########COLUMNS#####
@@ -113,7 +99,7 @@ for box in boxes:
 
 
 # step 3
-#displaying game visually
+# displaying game visually
 selector = html.DIV(id='selector_container' )
 selector <=(html.DIV(i, id=f'selector{i}', className='selector') for i in range(1, 10) )##list comprehension
 
@@ -125,135 +111,89 @@ section =(html.DIV('', id=box['name'],className='box') for box in boxes)##holds 
 container <= section
 document <= container
 
-###attach inner boxes to outer boxes
+# attach inner boxes to outer boxes
 for x in range(1,10):
     document[f'box{x}'] <= ( html.BUTTON('',id=f"{boxes[x-1]['name']}_inner_box{num}" ,className='inner_box') for num in range(1,10))
 
 
-
-
 #step 4
 ##creating game logic
-previous_id = []
-new_id = []
+id = []
+class_id = []
 
 def click_buttons(event):
     """
     perform an action when the button elements are clicked
     """
-    global previous_id ### to use the variable without producing an err
-    global new_id
+    global id ### to use the variable without producing an err
+    global class_id
     inner_box = event.target
     
-    if new_id == []  :
-        previous_id.append(inner_box.attrs['id'])
-        inner_box.attrs['id'] = 'selected'
-        new_id.append(inner_box.attrs['id'])
+    if class_id == []  :
+        id.append(inner_box.attrs['id'])
+        inner_box.class_name = 'selected'
+        class_id.append(inner_box.class_name)
         # print('run1')
-        if inner_box.text != '':
-    ####################################################
-            #slice out the number of the box from the id
-            box_num = previous_id[0][3:4]
-            #convert to int and minus 1 to get box index
-            box_index = int(box_num) -1 
-
-            py_inner_box = boxes[box_index][previous_id[0]]
-            #get column and row index
-            column_num = py_inner_box['column_attached'][-1]
-            column_index = int(column_num) -1
-
-            row_num = py_inner_box['row_attached'][-1]
-            row_index = int(row_num) -1
-
-    ######################################################
-            py_inner_box['value'] = ''
-            #note a slice will always return a new list (avoiding it below)
-            rows[row_index]['values'].remove(inner_box.text)
-            # print(rows[row_index]['values'])
-            columns[column_index]['values'].remove(inner_box.text)
-            # print(columns[column_index]['values'])
-            boxes[box_index]['values'].remove(inner_box.text)
-            # print(boxes[box_index]['values'])
-
-            inner_box.text = ''
+           
+        # clear all values from html,box,column and row
+        functions.clear_all_values(functions,id,inner_box,boxes,columns,rows,)
 
         if inner_box.class_name == 'error' :
             inner_box.class_name = ''
 
-    elif new_id[0] == inner_box.attrs['id']:
-        # for styling
-        inner_box.attrs['id'] = previous_id[0]
-        previous_id = []
-        new_id = []
+    elif class_id[0] == 'selected':
+        # for styling when another button is clicked
+        pre_btn = document[id[0]]
+        pre_btn.class_name = ''
+        inner_box.class_name = 'selected'
+        id[0]= inner_box.attrs['id']
+        class_id = [inner_box.class_name]
         # print('run2')
-    
-    elif new_id[0] != inner_box.attrs['id']:
-        # for styling 
-        previous_inner_box = document['selected'] 
-        previous_inner_box.attrs['id'] = previous_id[0]
-        previous_id[0] = inner_box.attrs['id']
-        inner_box.attrs['id'] = 'selected'
-        new_id[0] = inner_box.attrs['id']
-        # print('run3')
-
+        # clear all values from html,box,column and row
+        functions.clear_all_values(functions,id,inner_box,boxes,columns,rows,)
+  
+    #check if error class should be present
+    functions.unchecked_err(boxes,columns,rows)
         
         
-
-
 def click_selectors(event):
     """
     perform an action when the selectors are clicked
     """
-    global previous_id
-    global new_id
-    if new_id != []  :
+    global id
+    global class_id
+    if class_id != []  :
         selector = event.target
-        inner_box = document[new_id[0]]
-        inner_box.attrs['id'] = previous_id[0]
-       
-
-    #########################################################
-        #slice out the number of the box from the id
-        box_num = previous_id[0][3:4]
-        #convert to int and minus 1 to get box index
-        box_index = int(box_num) -1 
-        py_inner_box = boxes[box_index][previous_id[0]]
-        #get column and row index
-        column_num = py_inner_box['column_attached'][-1]
-        column_index = int(column_num) -1
-
-        row_num = py_inner_box['row_attached'][-1]
-        row_index = int(row_num) -1
-    ##########################################################
+        inner_box = document[id[0]]
+        inner_box.class_name = ''
+       #get the inner_box and its attached box,column,row
+        box_list = functions.get_inner_box_indexes(id,boxes)
 
         # ensure that the text value doesnt repeat in the column,row and boxes
         if inner_box.text == '':
             inner_box.text =  selector.text
 
         elif inner_box.text != '':
-            boxes[box_index]['values'].remove(inner_box.text)
-            columns[column_index]['values'].remove(inner_box.text) 
-            rows[row_index]['values'].remove(inner_box.text) 
+            boxes[box_list[1]]['values'].remove(inner_box.text)
+            columns[box_list[2]]['values'].remove(inner_box.text) 
+            rows[box_list[3]]['values'].remove(inner_box.text) 
             # attach the new text value
             inner_box.text = ''
             inner_box.text =  selector.text
 
 
         # attach the value of inner_box to column,row and box
-        py_inner_box['value'] = inner_box.text
-        boxes[box_index]['values'].append(inner_box.text)
-        columns[column_index]['values'].append(inner_box.text) 
-        rows[row_index]['values'].append(inner_box.text) 
+        box_list[0]['value'] = inner_box.text
+        boxes[box_list[1]]['values'].append(inner_box.text)
+        columns[box_list[2]]['values'].append(inner_box.text) 
+        rows[box_list[3]]['values'].append(inner_box.text) 
 
-        # print(rows[row_index])
-        # print(columns[column_index])
-        # print(boxes[box_index]['values'])
 
-        #check that no value is repeated in colums rows or boxes
+        # check that no value is repeated in colums rows or boxes
         for num in ['1','2','3','4','5','6','7','8','9']:
-            repeat_in_row = rows[row_index]['values'].count(num)
-            repeat_in_column = columns[column_index]['values'].count(num)
-            repeat_in_box = boxes[box_index]['values'].count(num)
+            repeat_in_box = boxes[box_list[1]]['values'].count(num)
+            repeat_in_column = columns[box_list[2]]['values'].count(num)
+            repeat_in_row = rows[box_list[3]]['values'].count(num)
 
             if (repeat_in_row > 1) \
                 or (repeat_in_column > 1) \
@@ -265,36 +205,19 @@ def click_selectors(event):
                     # print(inner_box.class_name)
                 
         # clear lists
-        previous_id = []
-        new_id = []
+        id = []
+        class_id = []
+    #check if error class should be present
+    functions.unchecked_err(boxes,columns,rows)
 
-    # step 5
-    # check if error class is still attached because number removed may not
-    # be the exact one that caused the error
-
-    for box in boxes:
-        for i in range(1,10):
-        # get inner_boxes name and check the class value
-            py_id =box[f"{box['name']}_inner_box{i}"]['name']
-            # to get the html equivalent
-            html_id = document[box[f"{box['name']}_inner_box{i}"]['name']]
-            # print(py_id)
-            if html_id.class_name == 'error':
-                column_index = int(box[py_id]['column_attached'][6:7]) - 1
-                row_index = int(box[py_id]['row_attached'][3:4]) - 1
-                #chexk for distinct values in CRB
-                if (len(box['values']) == len(set(box['values']))) \
-                and (len(columns[column_index]['values']) == len(set(columns[column_index]['values']))) \
-                and (len(rows[row_index]['values']) == len(set(rows[row_index]['values']))):
-                    html_id.class_name = ''
-                    print('nam me')
-
-
-### attaching click_button function to the button tag
+# attaching click_button function to the button tag
 for button in document.select("button"):# for tags list note
     button.bind("click", click_buttons)
-#####  attaching click_selectors function to the selectors
+# attaching click_selectors function to the selectors
 for value in range(1,10):
     selector = document[f'selector{value}']
     selector.bind("click", click_selectors)
+
+#run timer function
+
 
